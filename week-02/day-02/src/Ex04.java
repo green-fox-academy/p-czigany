97b6055f106f50e3393c0767686ac09af257d09e
Peter Czigany <p.czigany@gmail.com>
2017-03-21 13:10:54 +0100

    add excercises 4-6

---
+++ week-02/day-02/src/Ex04.java
@@ -0,0 +1,10 @@
+/**
+ * Created by peter on 2017.03.21..
+ */
+public class Ex04 {
+  public static void main(String[] args) {
+    System.out.println("Peter Czigany");
+    System.out.println("30");
+    System.out.println("1.83");
+  }
+}
---
+++ week-02/day-02/src/Ex05.java
@@ -0,0 +1,15 @@
+/**
+ * Created by peter on 2017.03.21..
+ */
+public class Ex05 {
+  public static void main(String[] args) {
+    int a = 22;
+    int b = 13;
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println((float) a / b);
+    System.out.println((int) a / b);
+    System.out.println(a % b);
+  }
+}
