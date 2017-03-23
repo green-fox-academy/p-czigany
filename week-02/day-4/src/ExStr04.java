47200962961d3991aa411e66185381983508465b
Peter Czigany <p.czigany@gmail.com>
2017-03-23 12:35:42 +0100

    add excercises w/ Strings

---
+++ week-02/day-4/src/ExStr04.java
@@ -0,0 +1,12 @@
+/**
+ * Created by peter on 2017.03.23..
+ */
+public class ExStr04 {
+
+  public static void main(String[] args) {
+    String todoText = " - Buy milk\n";
+    todoText = "My todo:\n" + todoText + " - Download games\n" + "\t\t - Diablo";
+    System.out.println(todoText);
+  }
+
+}
---
+++ week-02/day-4/src/ExStr05.java
@@ -0,0 +1,20 @@
+/**
+ * Created by peter on 2017.03.23..
+ */
+public class ExStr05 {
+
+  public static void main(String... args) {
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+    reversed = stringReverse(reversed);
+    System.out.println(reversed);
+  }
+
+  public static String stringReverse(String toReverse) {
+    String reversed = "";
+    for (int i = toReverse.length(); i > 0; i--) {
+      reversed += toReverse.charAt(i - 1);
+    }
+    return reversed;
+  }
+
+}
