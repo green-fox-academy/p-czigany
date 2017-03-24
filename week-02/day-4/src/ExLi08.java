3ccecfaebbca439890866de31fe027e5d59746a6
Peter Czigany <p.czigany@gmail.com>
2017-03-24 06:27:35 +0100

    add the 8th List excercise of day-4 (calculator)

---
+++ week-02/day-4/src/ExLi08.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Created by peter on 2017.03.23..
+ */
+public class ExLi08 {
+
+  public static void main(String... args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println(
+            "Give me some operation (+, -, *, /, %) and two operands (whole numbers), separated by spaces!");
+    String operation = scan.next();
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    if (operation.equals("+")) {
+      System.out.println(a + b);
+    } else if (operation.equals("-")) {
+      System.out.println(a - b);
+    } else if (operation.equals("*")) {
+      System.out.println(a * b);
+    } else if (operation.equals("/")) {
+      System.out.println(a / (float) b);
+    } else if (operation.equals("%")) {
+      System.out.println(a % b);
+    } else {
+      System.exit(0);
+    }
+
+  }
+
+}
