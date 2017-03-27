997c25e76b7c769314958ff808fd01b1cb14fa7c
Peter Czigany <p.czigany@gmail.com>
2017-03-27 20:35:40 +0200

    refactoring to functions

--- week-02/day-2/src/Ex29.java
+++ week-02/day-2/src/Ex29.java
@@ -1,42 +1,49 @@
-/**
- * Created by peter on 2017.03.21..
- */
-
 import java.util.Scanner;
 
+/**
+ * Created by peter on 2017.03.24..
+ */
 public class Ex29 {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int h = scan.nextInt();
-    boolean notEven = true;
-    if (h % 2 == 0) {
-      notEven = false;
-    }
+
     int greaterHalf;
-    if (notEven) {
-      greaterHalf = (h + 1) / 2;
-    } else {
+    if (h % 2 == 0) {
       greaterHalf = h / 2;
+    } else {
+      greaterHalf = (h + 1) / 2;
     }
+
+    printTriangle(greaterHalf);
+
+    printInverseTriangle(h, greaterHalf);
+
+  }
+
+  public static void printTriangle(int greaterHalf) {
     for (int i = 0; i < greaterHalf; i++) {
-      for (int j = 0; j < (greaterHalf - i); j++) {
-        System.out.print(" ");
-      }
-      System.out.print("*");
-      for (int j = 0; j < i; j++) {
-        System.out.print("**");
-      }
-      System.out.print("\n");
+      printRow(i, greaterHalf);
     }
-    for (int i = (h - greaterHalf) - 1; i > -1; i--) {
-      for (int j = 0; j < (greaterHalf - i); j++) {
-        System.out.print(" ");
-      }
-      System.out.print("*");
-      for (int j = 0; j < i; j++) {
-        System.out.print("**");
-      }
-      System.out.print("\n");
+  }
+
+  public static void
+  printInverseTriangle(int height, int greaterHalf) {
+    for (int i = (height - greaterHalf) - 1; i > -1; i--) {
+      printRow(i, greaterHalf);
     }
+  }
+
+  public static void
+  printRow(int i, int greaterHalf) {
+
+    for (int j = 0; j < (greaterHalf - i); j++) {
+      System.out.print(" ");
+    }
+    System.out.print("*");
+    for (int j = 0; j < i; j++) {
+      System.out.print("**");
+    }
+    System.out.print("\n");
   }
 }
