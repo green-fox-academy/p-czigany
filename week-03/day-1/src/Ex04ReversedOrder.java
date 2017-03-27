2db8d6b1696831f96865a3252304cb8bb22c941a
Peter Czigany <p.czigany@gmail.com>
2017-03-27 23:05:31 +0200

    add exercise 4 (reverse ordered lines)

---
+++ week-03/day-1/src/Ex04ReversedOrder.java
@@ -0,0 +1,53 @@
+import static java.nio.file.Files.readAllLines;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by peter on 2017.03.27..
+ */
+public class Ex04ReversedOrder {
+
+  public static void main(String[] args) {
+    reverseLinesToFile();
+  }
+
+  public static void reverseLinesToFile() {
+
+    List<String> linesFrom = readFromFile(Paths.get("assets/reversed-order.txt"));
+
+    List<String> linesTo = reverseLineOrder(linesFrom);
+
+    printToFile(Paths.get("assets/right-order.txt"), linesTo);
+  }
+
+  public static List<String> readFromFile(Path fileLocation) {
+    try {
+      return readAllLines(fileLocation);
+    } catch (IOException ex) {
+      System.out.println("I/O Exception occurred while trying to read from file.");
+      return null;
+    }
+  }
+
+  public static List<String> reverseLineOrder(List<String> toReverseOrder) {
+    List<String> reverseOrderedLines = new ArrayList<>();
+    for (int i = toReverseOrder.size(); i > 0; i--) {
+      reverseOrderedLines.add(toReverseOrder.get(i - 1));
+    }
+    return reverseOrderedLines;
+  }
+
+  public static void printToFile(Path filePath, List<String> toPrint) {
+    try {
+      Files.write(filePath, toPrint, Charset.forName("UTF-8"));
+    } catch (IOException ex) {
+      System.out.println("I/O Exception occurred while trying to write to a file.");
+    }
+  }
+}
