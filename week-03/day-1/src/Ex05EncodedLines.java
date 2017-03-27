e52cdf0f8f9e71095d15dcba36cef1e2c7d13add
Peter Czigany <p.czigany@gmail.com>
2017-03-27 23:40:36 +0200

    add exercise 5 (encoded lines)

---
+++ week-03/day-1/src/Ex05EncodedLines.java
@@ -0,0 +1,62 @@
+import static java.nio.file.Files.readAllLines;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by peter on 2017.03.27..
+ */
+public class Ex05EncodedLines {
+
+  public static void main(String[] args) {
+    decodeToFile();
+  }
+
+  public static void decodeToFile() {
+
+    List<String> linesFrom = readFromFile(Paths.get("assets/encoded-lines.txt"));
+
+    List<String> linesTo = decode(linesFrom);
+
+    printToFile(Paths.get("assets/decoded-lines.txt"), linesTo);
+  }
+
+  public static List<String> readFromFile(Path fileLocation) {
+    try {
+      return readAllLines(fileLocation);
+    } catch (IOException ex) {
+      System.out.println("I/O Exception occurred while trying to read from file.");
+      return null;
+    }
+  }
+
+  public static List<String> decode(List<String> toSingle) {
+    List<String> singledLines = new ArrayList<>();
+    for (String line :
+            toSingle) {
+      char[] charLine = line.toCharArray();
+      for (int i = 0; i < charLine.length; i++) {
+        if (charLine[i] != ' ') {
+          charLine[i] -= 1;
+        }
+      }
+      String oneLineTo = String.valueOf(charLine);
+      singledLines.add(oneLineTo);
+    }
+    return singledLines;
+  }
+
+  public static void printToFile(Path filePath, List<String> toPrint) {
+    try {
+      Files.write(filePath, toPrint, Charset.forName("UTF-8"));
+    } catch (IOException ex) {
+      System.out.println("I/O Exception occurred while trying to write to a file.");
+    }
+  }
+}
+
