89a38ceb32a9e152d138895b6edb2769011203d7
Peter Czigany <p.czigany@gmail.com>
2017-03-30 11:09:42 +0200

    change exercise 06 (odd bunny ear counter) bunny order
    odd indexed rabbits (1st, 3rd, 5th, etc.) have 2 ears and the even numbered have 3

--- week-03/day-4/src/Ex06BunniesAgain.java
+++ week-03/day-4/src/Ex06BunniesAgain.java
@@ -4,14 +4,14 @@
 public class Ex06BunniesAgain {
 
   public static void main(String[] args) {
-    System.out.println(oddBunnyEarCounter(6));
+    System.out.println(oddBunnyEarCounter(5));
   }
 
   public static int oddBunnyEarCounter(int bunnies) {
     if (bunnies < 1) {
       return 0;
     } else {
-      if (bunnies % 2 == 0) {
+      if (bunnies % 2 != 0) {
         return 2 + oddBunnyEarCounter(--bunnies);
       } else {
         return 3 + oddBunnyEarCounter(--bunnies);
