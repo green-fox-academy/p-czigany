d17aa1f325ae1f336aebabe07d62af0fb5c44bc3
Peter Czigany <p.czigany@gmail.com>
2017-03-30 11:33:51 +0200

    add initial commit of recursion exercise 08 (remove 'x's from a String)

---
+++ week-03/day-4/src/Ex08StringsAgain.java
@@ -0,0 +1,22 @@
+/**
+ * Created by peter on 2017.03.30..
+ */
+public class Ex08StringsAgain {
+
+  public static void main(String[] args) {
+    String input = "Roxxor the Xoxxor is BAAACK!!!";
+    System.out.println(xsRemove(input));
+  }
+
+  public static String xsRemove(String toTransform) {
+    if (toTransform.length() < 1) {
+      return "";
+    } else {
+      if (toTransform.charAt(0) == 'x') {
+        return xsRemove(toTransform.substring(1));
+      } else {
+        return String.valueOf(toTransform.charAt(0)) + xsRemove(toTransform.substring(1));
+      }
+    }
+  }
+}
