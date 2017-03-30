e1d7f99f9521f47b514318e23c46ddfd50c8f99d
Peter Czigany <p.czigany@gmail.com>
2017-03-30 10:11:45 +0200

    add initial commit of recursion exercise 1 (Counter)

---
+++ week-03/day-4/src/Ex01Counter.java
@@ -0,0 +1,19 @@
+/**
+ * Created by peter on 2017.03.30..
+ */
+public class Ex01Counter {
+
+  public static void main(String[] args) {
+    countdown(10);
+  }
+
+  public static int countdown(int highest) {
+    if (highest < 0) {
+      return 0;
+    } else {
+      System.out.println();
+      return
+    }
+  }
+
+}
