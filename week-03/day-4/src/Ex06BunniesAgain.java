86c5f7c92c9de44a94539a552e6854cd3a5860ad
Peter Czigany <p.czigany@gmail.com>
2017-03-30 11:06:38 +0200

    add initial commit of recursion exercise 06 (odd bunny ear counter)

---
+++ week-03/day-4/src/Ex06BunniesAgain.java
@@ -0,0 +1,21 @@
+/**
+ * Created by peter on 2017.03.30..
+ */
+public class Ex06BunniesAgain {
+
+  public static void main(String[] args) {
+    System.out.println(oddBunnyEarCounter(6));
+  }
+
+  public static int oddBunnyEarCounter(int bunnies) {
+    if (bunnies < 1) {
+      return 0;
+    } else {
+      if (bunnies % 2 == 0) {
+        return 2 + oddBunnyEarCounter(--bunnies);
+      } else {
+        return 3 + oddBunnyEarCounter(--bunnies);
+      }
+    }
+  }
+}
