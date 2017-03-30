853c83beeb936a3eeb1c1294c01b92c162ca1fdc
Peter Czigany <p.czigany@gmail.com>
2017-03-30 11:41:27 +0200

    add initial commit of recursion exercise 09 (add '*'s between each character of a String)

---
+++ week-03/day-4/src/Ex09StringsAgainAndAgain.java
@@ -0,0 +1,18 @@
+/**
+ * Created by peter on 2017.03.30..
+ */
+public class Ex09StringsAgainAndAgain {
+
+  public static void main(String[] args) {
+    String input = "Roxxor the Xoxxor is BAAACK!!!";
+    System.out.println(xsRemove(input));
+  }
+
+  public static String xsRemove(String toTransform) {
+    if (toTransform.length() < 2) {
+      return toTransform;
+    } else {
+      return String.valueOf(toTransform.charAt(0)) + "*" + xsRemove(toTransform.substring(1));
+    }
+  }
+}
