4f97c4958d27efa52f30583bb09ae6f1701665a7
Peter Czigany <p.czigany@gmail.com>
2017-04-03 14:54:01 +0200

    add initial commit of complex architectures exercise (Teachers and Students)

---
+++ week-04/day-1/src/Student.java
@@ -0,0 +1,15 @@
+/**
+ * Created by peter on 2017.04.03..
+ */
+public class Student {
+
+//  learn()
+//  question(teacher) -> calls the teachers answer method
+  public void learn() {
+
+  }
+
+  public void question(Teacher fromWho) {
+    fromWho.answer();
+  }
+}
---
+++ week-04/day-1/src/Teacher.java
@@ -0,0 +1,14 @@
+/**
+ * Created by peter on 2017.04.03..
+ */
+public class Teacher {
+
+//  teach(student) -> calls the students learn method
+//  answer()
+  public void teach(Student whom) {
+    whom.learn();
+  }
+  public void answer() {
+
+  }
+}
