40d50b3ee4f41d5a05dec752267864657179d65d
Peter Czigany <p.czigany@gmail.com>
2017-04-03 12:30:01 +0200

    add initial commit of use class exercise (Dices)

---
+++ week-04/day-1/src/Dice.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+/**
+ * Created by peter on 2017.04.03..
+ */
+public class Dice {
+//    You have a Dice class which has 6 dice
+//    You can roll all of them with roll()
+//    Check the current rolled numbers with getCurrent()
+//    You can reroll with reroll()
+//    Your task is to get where all dice is a 6
+  int[] dices = new int[6];
+
+  public int[] roll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+    return dices;
+  }
+
+  public int[] getCurrent() {
+    return dices;
+  }
+
+  public int getCurrent(int i) {
+    return dices[i];
+  }
+
+  public void reroll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+  }
+
+  public void reroll(int k) {
+    dices[k] = (int) (Math.random() * 6) + 1;
+  }
+
+  public static void main(String[] args) {
+    Dice myDice = new Dice();
+    while (!Arrays.toString(myDice.getCurrent()).equals("[6, 6, 6, 6, 6, 6]")) {
+      myDice.reroll();
+    }
+    System.out.println(Arrays.toString(myDice.getCurrent()));
+  }
+}
