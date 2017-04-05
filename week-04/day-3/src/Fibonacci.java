09d6090674b37517320bf7d251778ffc90d0807c
Peter Czigany <p.czigany@gmail.com>
2017-04-05 13:05:45 +0200

    Add initial commit of testing exercise method (Fibonacci)

---
+++ week-04/day-3/src/Fibonacci.java
@@ -0,0 +1,15 @@
+/**
+ * Created by peter on 2017.04.05..
+ */
+public class Fibonacci {
+
+  int fibonacci(int nth) {
+    if (nth == 1) {
+      return 0;
+    } else if (nth == 2) {
+      return 1;
+    } else {
+      return fibonacci(nth - 1) + fibonacci(nth - 2);
+    }
+  }
+}
