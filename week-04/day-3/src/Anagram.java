ebb5f95a4fd83b439184739f3e57af93d5d7383d
Peter Czigany <p.czigany@gmail.com>
2017-04-05 12:01:55 +0200

    Add initial commit of the class of the tested method of test exercise (Anagram)

---
+++ week-04/day-3/src/Anagram.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by peter on 2017.04.05..
+ */
+public class Anagram {
+
+  boolean areAnagrams(String firstString, String secondString) {
+    {
+      ArrayList<Character> firstCharacters = stringToLetters(firstString);
+      ArrayList<Character> secondCharacters = stringToLetters(secondString);
+      return (firstCharacters.containsAll(secondCharacters) && secondCharacters
+              .containsAll(firstCharacters));
+    }
+  }
+
+  ArrayList<Character> stringToLetters(String toConvert) {
+    toConvert = toConvert.replaceAll("[^a-zA-Z]", "").toLowerCase();
+    ArrayList<Character> converted = new ArrayList<>();
+    for (int i = 0; i < toConvert.length(); i++) {
+      converted.add(toConvert.charAt(i));
+    }
+    return converted;
+  }
+}
