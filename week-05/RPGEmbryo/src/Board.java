29731415e44fa7704fb049f30459fefea3156c4d
Peter Czigany <p.czigany@gmail.com>
2017-04-11 22:23:55 +0200

    Add initial commit of RPGGame
     - Add functionality: draw a single floor tile

---
+++ week-05/RPGEmbryo/src/Board.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Created by peter on 2017.04.10..
+ */
+public class Board extends JComponent implements KeyListener {
+
+  int testBoxX;
+  int testBoxY;
+
+  public Board() {
+    testBoxX = 300;
+    testBoxY = 300;
+
+    // set the size of your draw board
+    setPreferredSize(new Dimension(72, 72));
+    setVisible(true);
+  }
+
+  @Override
+  public void paint(Graphics graphics) {
+    super.paint(graphics);
+
+    // here you have a 720x720 canvas
+    // you can create and draw an image using the class below e.g.
+
+    PositionedImage image = new PositionedImage("assets/floor.png", 0, 0);
+    image.draw(graphics);
+  }
+
+  // To be a KeyListener the class needs to have these 3 methods in it
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+  // But actually we can use just this one for our goals here
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // When the up or down keys hit, we change the position of our box
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      testBoxY -= 100;
+    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+      testBoxY += 100;
+    }
+    // and redraw to have a new picture with the new coordinates
+    repaint();
+  }
+}
