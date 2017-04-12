ca97349364abb958119972112b3f938925ab635f
Peter Czigany <p.czigany@gmail.com>
2017-04-12 12:15:55 +0200

    Rename class PositionedImage to GameObject

--- week-05/RPGEmbryo/src/GameObject.java
+++ week-05/RPGEmbryo/src/GameObject.java
@@ -1,18 +1,15 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by peter on 2017.04.10..
  */
-public class PositionedImage {
+public class GameObject {
 
   private BufferedImage image;
   private int posX, posY;
 
-  public PositionedImage(BufferedImage image, int posX, int posY) {
+  public GameObject(BufferedImage image, int posX, int posY) {
     this.posX = posX;
     this.posY = posY;
     this.image = image;
