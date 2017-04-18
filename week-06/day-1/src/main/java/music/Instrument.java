5f4f29931b4dc39c36d26710469f9c3df0e1cbae
Peter Czigany <p.czigany@gmail.com>
2017-04-18 16:19:57 +0200

    Add Instrument abstract class w/ the specified field + sound

---
+++ week-06/day-1/src/main/java/music/Instrument.java
@@ -0,0 +1,12 @@
+package main.java.music;
+
+/**
+ * Created by peter on 2017.04.18..
+ */
+public abstract class Instrument {
+
+  protected String name;
+  protected String sound;
+
+  public abstract void play();
+}
