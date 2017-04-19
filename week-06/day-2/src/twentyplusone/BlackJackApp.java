3f15ecd74f6aef958c31dd0ba57c3a91b118848b
Peter Czigany <p.czigany@gmail.com>
2017-04-19 16:06:16 +0200

    Refactor from the main to the Game class

---
+++ week-06/day-2/src/twentyplusone/BlackJackApp.java
@@ -0,0 +1,11 @@
+package twentyplusone;
+
+/**
+ * Created by peter on 2017.04.19..
+ */
+public class BlackJackApp {
+
+  public static void main(String[] args) {
+    Game myGame = new Game();
+  }
+}
