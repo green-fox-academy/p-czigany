28d6562a944c9d03f40f2e4823e46941c3951181
Peter Czigany <p.czigany@gmail.com>
2017-04-19 16:17:42 +0200

    Add fill method to the Deck constructor which creates a deck of Card class objects from random cards of a single 52-card deck

--- week-06/day-2/src/twentyplusone/Card.java
+++ week-06/day-2/src/twentyplusone/Card.java
@@ -5,8 +5,29 @@ package twentyplusone;
  */
 public class Card {
 
-  CardColor color;
-  CardSuit suit;
-  CardRank rank;
+  private CardColor color;
+  private CardSuit suit;
+  private CardRank rank;
 
+  public Card(CardRank rank, CardSuit suit) {
+    this.suit = suit;
+    if (suit.equals(CardSuit.CLUBS) && suit.equals(CardSuit.SPADES)) {
+      color = CardColor.BLACK;
+    } else {
+      color = CardColor.RED;
+    }
+    this.rank = rank;
+  }
+
+  public CardSuit getSuit() {
+    return suit;
+  }
+
+  public CardRank getRank() {
+    return rank;
+  }
+
+  public String printCard() {
+    return rank.name() + " " + suit.name();
+  }
 }
--- week-06/day-2/src/twentyplusone/Deck.java
+++ week-06/day-2/src/twentyplusone/Deck.java
@@ -10,8 +10,9 @@ public class Deck {
 
   private List<Card> cards;
 
-  public Deck() {
+  public Deck(int numberOfCards) {
     this.cards = new ArrayList<>();
+    fill(numberOfCards);
   }
 
   public void shuffleDeck() {
@@ -33,5 +34,73 @@ public class Deck {
   public Card pullRandom() {
     int randomIndex = (int) (Math.random() * cards.size());
     return cards.remove(randomIndex);
+  }
+
+  public void fill(int numberOfCards) {
+    for (int i = 0; i < numberOfCards; i++) {
+      while (cards.size() < i + 1) {
+        Card toAdd = randomCard();
+        boolean addable = true;
+        if (i > 0) {
+          for (Card examinedCard :
+                  cards) {
+            if (toAdd.printCard().equals(examinedCard.printCard())) {
+              addable = false;
+            }
+          }
+        }
+        if (addable) {
+          cards.add(toAdd);
+        }
+      }
+    }
+  }
+
+  public CardRank randomRank() {
+    int d13 = (int) (Math.random() * 13) + 1;
+    if (d13 == 1) {
+      return CardRank.ACE;
+    } else if (d13 == 2) {
+      return CardRank.TWO;
+    } else if (d13 == 3) {
+      return CardRank.THREE;
+    } else if (d13 == 4) {
+      return CardRank.FOUR;
+    } else if (d13 == 5) {
+      return CardRank.FIVE;
+    } else if (d13 == 6) {
+      return CardRank.SIX;
+    } else if (d13 == 7) {
+      return CardRank.SEVEN;
+    } else if (d13 == 8) {
+      return CardRank.EIGHT;
+    } else if (d13 == 9) {
+      return CardRank.NINE;
+    } else if (d13 == 10) {
+      return CardRank.TEN;
+    } else if (d13 == 11) {
+      return CardRank.JACK;
+    } else if (d13 == 12) {
+      return CardRank.QUEEN;
+    } else {
+      return CardRank.KING;
+    }
+  }
+
+  public CardSuit randomSuit() {
+    int d4 = (int) (Math.random() * 4) + 1;
+    if (d4 == 1) {
+      return CardSuit.SPADES;
+    } else if (d4 == 2) {
+      return CardSuit.HEARTS;
+    } else if (d4 == 3) {
+      return CardSuit.DIAMONDS;
+    } else {
+      return CardSuit.CLUBS;
+    }
+  }
+
+  public Card randomCard() {
+    return new Card(randomRank(), randomSuit());
   }
 }
