8f93ad6b821c6c05e94d429155ed027aa5f4a240
Peter Czigany <p.czigany@gmail.com>
2017-04-19 21:14:23 +0200

    Add exception handling

--- week-06/day-2/src/sorthearray/ArraySorter.java
+++ week-06/day-2/src/sorthearray/ArraySorter.java
@@ -11,13 +11,17 @@ public class ArraySorter {
     T temp;
     while (flag) {
       flag = false;
-      for (j = 0; j < inputArray.length - 1; j++) {
-        if (inputArray[j].doubleValue() < inputArray[j + 1].doubleValue()) {
-          temp = inputArray[j];
-          inputArray[j] = inputArray[j + 1];
-          inputArray[j + 1] = temp;
-          flag = true;
+      try {
+        for (j = 0; j < inputArray.length - 1; j++) {
+          if (inputArray[j].doubleValue() < inputArray[j + 1].doubleValue()) {
+            temp = inputArray[j];
+            inputArray[j] = inputArray[j + 1];
+            inputArray[j + 1] = temp;
+            flag = true;
+          }
         }
+      } catch (NullPointerException e) {
+        System.out.println("Null pointer exception!");
       }
     }
   }
