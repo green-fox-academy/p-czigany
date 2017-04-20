5cafcd46b244a3b3c8a36a2fa2e0a903838274ba
Peter Czigany <p.czigany@gmail.com>
2017-04-20 22:38:34 +0200

    Create functionality: filter table lines by year

--- week-06/day-3/lottery/src/main/java/lottery/LotteryApp.java
+++ week-06/day-3/lottery/src/main/java/lottery/LotteryApp.java
@@ -1,6 +1,9 @@
 package lottery;
 
+import java.util.ArrayList;
 import java.util.List;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 /**
  * Created by peter on 2017.04.20..
@@ -8,10 +11,24 @@ import java.util.List;
 public class LotteryApp {
 
   public static void main(String[] args) {
+    OptionParser parser = new OptionParser();
+    parser.accepts("y").withRequiredArg();
+    OptionSet options = parser.parse(args);
     String sourcePath = "assets/otos.csv";
     CsvFileHandler csvFiles = new CsvFileHandler();
     List<String[]> tableLines = csvFiles.getTableLines(sourcePath);
-    List<String[]> linesToPrint = tableLines;
+    List<String[]> linesToPrint = new ArrayList<>();
+    if (options.has("y")) {
+      String year = options.valueOf("y").toString();
+      for (String[] line :
+              tableLines) {
+        if (line[0].equals(year)) {
+          linesToPrint.add(line);
+        }
+      }
+    } else {
+      linesToPrint = tableLines;
+    }
     String targetPath = "assets/output.csv";
     csvFiles.writeTableLines(targetPath, linesToPrint);
   }
