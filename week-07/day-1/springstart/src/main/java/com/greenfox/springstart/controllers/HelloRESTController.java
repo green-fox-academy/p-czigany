68a6a1887d4098a3ed6a73f0543a07925077215d
Peter Czigany <p.czigany@gmail.com>
2017-05-02 12:34:20 +0200

    Add functionality: Hello User REST App

--- week-07/day-1/springstart/src/main/java/com/greenfox/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfox/springstart/controllers/HelloRESTController.java
@@ -1,6 +1,8 @@
 package com.greenfox.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRESTController {
 
   @RequestMapping(value = "/greeting")
-  public Greeting greeting() {
-    return new Greeting(1, "Hello World!");
+  public Greeting greeting(@RequestParam("name") String name) {
+    return new Greeting(1, "Hello " + name + "!");
   }
 }
