67a0be7ded4ac5d8bff640a7b02b860fe7efb650
Peter Czigany <p.czigany@gmail.com>
2017-05-03 19:38:34 +0200

    Use table formatting in the .html template

---
+++ week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/controller/Ex10BankAccountController.java
@@ -0,0 +1,35 @@
+package com.greenfoxacademy.thstart.controller;
+
+import com.greenfoxacademy.thstart.model.BankAccount3;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by peter on 2017.05.03..
+ */
+@Controller
+public class Ex10BankAccountController {
+
+  @RequestMapping(value = "/ex10")
+  public String accountOfClient(@RequestParam("client") int index, Model model) {
+    ArrayList<BankAccount3> bankAccountList = new ArrayList<>();
+    bankAccountList.add(new BankAccount3("Nala", 3000, "lion", false, true));
+    bankAccountList.add(new BankAccount3("Rafiki", 500, "mandrill", false, true));
+    bankAccountList.add(new BankAccount3("Zazu", 3000, "red-billed hornbill", false, true));
+    bankAccountList.add(new BankAccount3("Mufasa", 0, "dead lion", false, true));
+    bankAccountList.add(new BankAccount3("Simba", 2000, "lion", true, true));
+    bankAccountList.add(new BankAccount3("Timon", 300000, "meerkat", false, true));
+    bankAccountList.add(new BankAccount3("Scar", 3000, "lion", false, false));
+    BankAccount3 account = bankAccountList.get(index - 1);
+    model.addAttribute("isGood", account.isGood());
+    model.addAttribute("isKing", account.isKing());
+    model.addAttribute("index", index);
+    model.addAttribute("name", account.getName());
+    model.addAttribute("balance", account.getBalance());
+    model.addAttribute("animalType", account.getAnimalType());
+    return "bankaccount10";
+  }
+}
