9b6bd85a56bb4c16196bd9fd973c622a4b51ca83
Peter Czigany <p.czigany@gmail.com>
2017-05-03 13:00:10 +0200

    Add functionality: show decimal places
     - Add changed template
     - Add controller for reaching the changed template

---
+++ week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/controller/Ex02Controller.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.thstart.controller;
+
+import com.greenfoxacademy.thstart.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by peter on 2017.05.03..
+ */
+@Controller
+public class Ex02Controller {
+
+  @RequestMapping(value = "/ex02")
+  public String simba(Model model) {
+    BankAccount lionKing = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute("name", lionKing.getName());
+    model.addAttribute("balance", lionKing.getBalance());
+    model.addAttribute("animalType", lionKing.getAnimalType());
+    return "bankaccount02";
+  }
+}
