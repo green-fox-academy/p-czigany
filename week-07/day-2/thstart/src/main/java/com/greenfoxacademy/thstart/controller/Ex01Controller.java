409ef8e3d59877aa89362ff13a1e9de9615fef42
Peter Czigany <p.czigany@gmail.com>
2017-05-03 12:42:11 +0200

    Add EndPoint
     - Create new BankAccount
     - Add BankAccount fields to model
     - Return with the filled template

--- week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/controller/Ex01Controller.java
+++ week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/controller/Ex01Controller.java
@@ -1,6 +1,9 @@
 package com.greenfoxacademy.thstart.controller;
 
+import com.greenfoxacademy.thstart.model.BankAccount;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by peter on 2017.05.03..
@@ -8,4 +11,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Ex01Controller {
 
+  @RequestMapping(value = "/new")
+  public String simba(Model model) {
+    BankAccount lionKing = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute("name", lionKing.getName());
+    model.addAttribute("balance", lionKing.getBalance());
+    model.addAttribute("animalType", lionKing.getAnimalType());
+    return "bankaccount";
+  }
 }
