2eee9bd642f38375d48c2299bbce3a84c38b9c7a
Peter Czigany <p.czigany@gmail.com>
2017-05-03 16:08:51 +0200

    Add index number to display

---
+++ week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/controller/Ex06BankAccountController.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.thstart.controller;
+
+import com.greenfoxacademy.thstart.model.BankAccount;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by peter on 2017.05.03..
+ */
+@Controller
+public class Ex06BankAccountController {
+
+  @RequestMapping(value = "/ex06")
+  public String accountOfClient(@RequestParam("client") int index, Model model) {
+    ArrayList<BankAccount> bankAccountList = new ArrayList<>();
+    bankAccountList.add(new BankAccount("Nala", 3000, "lion"));
+    bankAccountList.add(new BankAccount("Rafiki", 500, "mandrill"));
+    bankAccountList.add(new BankAccount("Zazu", 3000, "red-billed hornbill"));
+    bankAccountList.add(new BankAccount("Mufasa", 0, "dead lion"));
+    bankAccountList.add(new BankAccount("Timon", 300000, "meerkat"));
+    BankAccount account = bankAccountList.get(index - 1);
+    model.addAttribute("index", index);
+    model.addAttribute("name", account.getName());
+    model.addAttribute("balance", account.getBalance());
+    model.addAttribute("animalType", account.getAnimalType());
+    return "bankaccount06";
+  }
+}
