dd7b74ad44afa61f5ced1696c858e5c8fcca7d9e
Peter Czigany <p.czigany@gmail.com>
2017-05-03 13:05:02 +0200

    Add the word 'Zebra' after the displayed balance
     - Add changed template
     - Add controller for reaching the changed template

---
+++ week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/controller/Ex03Controller.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.thstart.controller;
+
+import com.greenfoxacademy.thstart.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by peter on 2017.05.03..
+ */
+@Controller
+public class Ex03Controller {
+
+  @RequestMapping(value = "/ex03")
+  public String simba(Model model) {
+    BankAccount lionKing = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute("name", lionKing.getName());
+    model.addAttribute("balance", lionKing.getBalance());
+    model.addAttribute("animalType", lionKing.getAnimalType());
+    return "bankaccount03";
+  }
+}
