9a638d33dbb4c9237657c12613dea45f5fb74494
Peter Czigany <p.czigany@gmail.com>
2017-05-03 11:51:09 +0200

    Add model constructor

--- week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/model/BankAccount.java
+++ week-07/day-2/thstart/src/main/java/com/greenfoxacademy/thstart/model/BankAccount.java
@@ -1,11 +1,15 @@
 package com.greenfoxacademy.thstart.model;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
-
 /**
  * Created by peter on 2017.05.03..
  */
 public class BankAccount {
   private String name, animalType;
   private int balance;
+
+  public BankAccount(String name, int balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
 }
