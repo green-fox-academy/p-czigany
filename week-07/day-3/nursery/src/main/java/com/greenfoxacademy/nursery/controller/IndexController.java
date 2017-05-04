5bef09628a7ea741d1992e5e59419efaf7a96dd8
Peter Czigany <p.czigany@gmail.com>
2017-05-04 17:26:41 +0200

    Add initial commit of Fox Nursery
     - Load a static html

---
+++ week-07/day-3/nursery/src/main/java/com/greenfoxacademy/nursery/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.nursery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by peter on 2017.05.04..
+ */
+//@Controller
+//public class IndexController {
+//
+//  @RequestMapping(value = "/")
+//  public String index() {
+//    return "index";
+//  }
+//}
