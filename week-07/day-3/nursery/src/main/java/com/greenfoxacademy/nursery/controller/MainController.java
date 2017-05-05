e313bbc6df45ba0a1602a93abcf1f39f150e2abe
Peter Czigany <p.czigany@gmail.com>
2017-05-05 11:00:57 +0200

    Make index page dinamically display properties of the fox class
     - Display name, food, drink, # of tricks
     - Display inexperienced message if # of tricks = 0
     - Display list of tricks if # of tricks != 0

--- week-07/day-3/nursery/src/main/java/com/greenfoxacademy/nursery/controller/MainController.java
+++ week-07/day-3/nursery/src/main/java/com/greenfoxacademy/nursery/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.greenfoxacademy.nursery.controller;
 
+import com.greenfoxacademy.nursery.config.ModelAndViewFactory;
+import com.greenfoxacademy.nursery.model.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by peter on 2017.05.04..
@@ -9,8 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-  @RequestMapping(value = "/")
-  public String index() {
-    return "index";
+  @Autowired
+  ModelAndViewFactory modelAndViewFactory;
+  @Autowired
+  Fox fox;
+
+  @RequestMapping("/")
+  public ModelAndView index() {
+    ModelAndView m = modelAndViewFactory.getModelAndView();
+    m.setViewName("index");
+    m.addObject("fox", fox);
+    return m;
   }
 }
