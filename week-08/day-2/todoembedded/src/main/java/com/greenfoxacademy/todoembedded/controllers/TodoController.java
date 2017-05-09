43921bbc669384ca3f358293eab69b4cb7431f9b
Peter Czigany <p.czigany@gmail.com>
2017-05-09 12:10:54 +0200

    Add template and call it from the controller on EndPoint(/, /todo)

--- week-08/day-2/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/controllers/TodoController.java
+++ week-08/day-2/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/controllers/TodoController.java
@@ -3,9 +3,7 @@ package com.greenfoxacademy.todoembedded.controllers;
 import com.greenfoxacademy.todoembedded.model.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by peter on 2017.05.09..
@@ -16,9 +14,8 @@ public class TodoController {
   @Autowired
   private TodoRepository repository;
 
-  @ResponseBody
   @RequestMapping(value = {"/", "/list"})
-  public String list(Model model) {
-    return "This is my first todo";
+  public String list() {
+    return "todo";
   }
 }
