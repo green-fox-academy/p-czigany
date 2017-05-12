58493cf8ab7a49f7d16f8c1f8fc2d9fecb6fd54c
Peter Czigany <p.czigany@gmail.com>
2017-05-12 10:32:03 +0200

    Fix Post model entity with getters & setters

--- week-08/day-5/reddit/src/main/java/com/greenfox/pczigany/reddit/models/Post.java
+++ week-08/day-5/reddit/src/main/java/com/greenfox/pczigany/reddit/models/Post.java
@@ -20,4 +20,43 @@ public class Post {
   Timestamp timestamp;
   int score;
 
+  public long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getHref() {
+    return href;
+  }
+
+  public Timestamp getTimestamp() {
+    return timestamp;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setHref(String href) {
+    this.href = href;
+  }
+
+  public void setTimestamp(Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
 }
