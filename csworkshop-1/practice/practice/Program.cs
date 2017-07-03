6d8132e746e3a6f5df604fb4e6bbe074eaeef3d8
Peter Czigany <p.czigany@gmail.com>
2017-07-03 11:54:44 +0200

    Refactor functionality
    - Get it out from the Main method
    - Make it a static method next to Main method

--- csworkshop-1/practice/practice/Program.cs
+++ csworkshop-1/practice/practice/Program.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace HelloWorld
+namespace practice
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
-            IEnumerable<int> evenNumbers = n.Where(y => y % 2 == 0);
+        }
+
+        public static void printEvenNumbers(int[] inputNumbers)
+        {
+            IEnumerable<int> evenNumbers = inputNumbers.Where(y => y % 2 == 0);
             foreach (int i in evenNumbers)
             {
                 Console.WriteLine(i);
