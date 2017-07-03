3084a389061784393bc00494a2b281cecb13c0a7
Peter Czigany <p.czigany@gmail.com>
2017-07-03 11:44:49 +0200

    Add exercise with LINQ filtering an array

--- csworkshop-1/practice/practice/Program.cs
+++ csworkshop-1/practice/practice/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace practice
+namespace HelloWorld
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            IEnumerable<int> evenNumbers = n.Where(y => y % 2 == 0);
+            foreach (int i in evenNumbers)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
