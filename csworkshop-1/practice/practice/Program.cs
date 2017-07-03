18844d63f8aaec29cfd34001d38796a74464abc8
Peter Czigany <p.czigany@gmail.com>
2017-07-03 11:59:10 +0200

    Add LINQ exercise w/ averaging

--- csworkshop-1/practice/practice/Program.cs
+++ csworkshop-1/practice/practice/Program.cs
@@ -8,6 +8,8 @@ namespace practice
     {
         public static void Main(string[] args)
         {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            printAverage(n);
         }
 
         public static void printEvenNumbers(int[] inputNumbers)
@@ -17,6 +19,13 @@ namespace practice
             {
                 Console.WriteLine(i);
             }
+        }
+
+        public static void printAverage(int[] inputNumbers)
+        {
+            double oddAverage = inputNumbers.Where(y => y % 2 != 0)
+                                            .Average();
+            Console.WriteLine(oddAverage);           
         }
     }
 }
