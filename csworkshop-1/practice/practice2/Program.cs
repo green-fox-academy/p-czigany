30e781b997edd9ba1005b8ea86aea419d64d4895
Peter Czigany <p.czigany@gmail.com>
2017-07-03 12:28:46 +0200

    Refactor 3 functions to 3 projects

---
+++ csworkshop-1/practice/practice2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ csworkshop-1/practice/practice3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
