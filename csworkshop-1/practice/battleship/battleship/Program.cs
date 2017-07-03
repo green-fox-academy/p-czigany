8783fa9436b4f8d98d8c3ec121b491569a5e11eb
Peter Czigany <p.czigany@gmail.com>
2017-07-03 14:03:19 +0200

    Set up useful constants

--- csworkshop-1/practice/battleship/battleship/Program.cs
+++ csworkshop-1/practice/battleship/battleship/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace battleship
 {
     class MainClass
     {
+        static int MAPSIZE = 20;
+        static int[,] map = new int[MAPSIZE, MAPSIZE];
+        static int[,] appearedMap = new int[MAPSIZE, MAPSIZE];
+        //column names:
+        const List<string> ALPHABET = new List<string>
+        {   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+        //# of ships by length:
+        const Dictionary<int, int> SHIPS = new Dictionary<int, int>
+        {   { 8, 1 }, 
+            { 5, 2 }, 
+            { 3, 3 }, 
+            { 2, 3 }, 
+            { 1, 4 }
+        };
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
