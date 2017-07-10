4e6be6cf39453b007850e5111f60ff298824ac00
Peter Czigany <p.czigany@gmail.com>
2017-07-10 10:18:48 +0200

    Not iterate through the characters twice

--- finalexamjumpin/src/PalindromeFinder.java
+++ finalexamjumpin/src/PalindromeFinder.java
@@ -4,8 +4,10 @@ class PalindromeFinder {
     String withoutPunctuationOrSpaces = examinedText.replaceAll("[^a-zA-Z0-9]", "");
     String noPunctuationLowerCase = withoutPunctuationOrSpaces.toLowerCase();
     char[] smallCaseLetters = noPunctuationLowerCase.toCharArray();
-    for (int i = 0; i < smallCaseLetters.length; i++) {
-      if (smallCaseLetters[i] != smallCaseLetters[smallCaseLetters.length - 1 - i]) {
+    int characters = smallCaseLetters.length;
+    int compareUpTo = (characters % 2 == 0) ? characters / 2 : (characters + 1) /2;
+    for (int i = 0; i < compareUpTo; i++) {
+      if (smallCaseLetters[i] != smallCaseLetters[characters - 1 - i]) {
         return false;
       }
     }
