b171e2baae1f26aa42bec90a728bfdd16af254b6
Peter Czigany <p.czigany@gmail.com>
2017-07-11 14:31:05 +0200

    Add solution to letter counter exercise

---
+++ csworkshop-2/practice/CountLetters/CountLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountLetters
+{
+    public class CountLetters
+    {
+        public Dictionary<char, int> LetterCount(string toCount)
+        {
+            if (toCount == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var characters = new Dictionary<char, int>();
+            foreach (var character in toCount.ToCharArray())
+            {
+                if (!characters.ContainsKey(character))
+                {
+                    characters.Add(character, 0);
+                }
+                int currentValue;
+                characters.TryGetValue(character, out currentValue);
+                characters.Add(character, currentValue + 1);
+            }
+            return characters;
+        }
+    }
+}
