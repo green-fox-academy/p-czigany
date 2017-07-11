fe0509825d8cbfd20754e67d5a81d820d97efffc
Peter Czigany <p.czigany@gmail.com>
2017-07-11 11:48:02 +0200

    Add solution to anagram exercise

---
+++ csworkshop-2/practice/anagram/Anagram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace anagram
+{
+    public class Anagram
+    {
+        public static bool AreAnagrams(string firstOne, string secondOne)
+        {
+            List<char> lettersInFirst = MakeLetterList(firstOne);
+            List<char> lettersInSecond = MakeLetterList(secondOne);
+            if (lettersInFirst.Count != lettersInSecond.Count)
+            {
+                return false;
+            }
+            if (RemoveLetters(lettersInFirst, lettersInSecond).Count != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static List<char> MakeLetterList(string stringToDeconstruct)
+        {
+            string lowerCase = stringToDeconstruct.ToLower();
+            string noPunctuation = Regex.Replace(lowerCase, "[^a-z0-9]", "");
+            char[] onlyLetters = noPunctuation.ToCharArray();
+
+            List<char> stringAsList = new List<char>();
+            foreach (var letter in onlyLetters)
+            {
+                stringAsList.Add(letter);
+            }
+            return stringAsList;
+        }
+
+        private static List<char> RemoveLetters(List<char> toRemove, List<char> removeFrom)
+        {
+            foreach (var letter in toRemove)
+            {
+                if (removeFrom.Contains(letter))
+                {
+                    removeFrom.Remove(letter);
+                }
+            }
+            return removeFrom;
+        }
+    }
+}
---
+++ csworkshop-2/practice/anagram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace anagram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in two strings. Press Enter after each.");
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+            if (Anagram.AreAnagrams(firstString, secondString))
+            {
+                Console.WriteLine("YES, they are anagrams indeed!");
+            }
+            else
+            {
+                Console.WriteLine("NO, they are not anagrams.");
+            }
+        }
+    }
+}
