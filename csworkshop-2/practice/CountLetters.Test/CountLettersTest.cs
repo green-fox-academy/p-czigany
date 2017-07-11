624e5940b329588871606499ef38011e564ab84e
Peter Czigany <p.czigany@gmail.com>
2017-07-11 14:56:07 +0200

    Add test cases to lettercounter

---
+++ csworkshop-2/practice/CountLetters.Test/CountLettersTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using CountLetters;
+
+using System.Collections.Generic;
+
+namespace CountLetters.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void StringInput_Test()
+        {
+            string inputText = "dfsfdeF?$";
+            Dictionary<char, int> expected = new Dictionary<char, int>()
+            {
+                {'d', 2},
+                {'f', 2},
+                {'s', 1},
+                {'e', 1},
+                {'F', 1},
+                {'?', 1},
+                {'$', 1}
+            };
+            Assert.AreEqual(expected, CountLetters.LetterCount(inputText));
+        }
+
+        [Test()]
+        public void StringInputDifferentOrder_Test()
+        {
+            string inputText = "dfsfdeF?$";
+            Dictionary<char, int> expected = new Dictionary<char, int>()
+            {
+                {'f', 2},
+                {'s', 1},
+                {'e', 1},
+                {'F', 1},
+                {'?', 1},
+                {'$', 1},
+                {'d', 2}
+            };
+            Assert.AreEqual(expected, CountLetters.LetterCount(inputText));
+        }
+
+        [Test()]
+        public void StringInputWithWrongExpectation_Test()
+        {
+            string inputText = "dfsfdeF?$";
+            Dictionary<char, int> expected = new Dictionary<char, int>()
+            {
+                {'d', 1},
+                {'f', 2},
+                {'s', 1},
+                {'e', 1},
+                {'F', 2},
+                {'?', 1},
+                {'$', 1}
+            };
+            Assert.AreNotEqual(expected, CountLetters.LetterCount(inputText));
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullInput_Test()
+        {
+            string inputText = null;
+            Dictionary<char, int> expected = new Dictionary<char, int>();
+            CountLetters.LetterCount(inputText);
+        }
+    }
+}
