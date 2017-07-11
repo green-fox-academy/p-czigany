062787e5b685458c2c371f4704e580c998ae6c1c
Peter Czigany <p.czigany@gmail.com>
2017-07-11 12:47:33 +0200

    Add test project w/ test cases

---
+++ csworkshop-2/practice/Anagram.Test/AnagramTest.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+using Anagram;
+
+namespace Anagram.Test
+{
+    [TestFixture()]
+    public class AnagramTest
+    {
+        [Test()]
+        public void AnagramInputs_Test()
+        {
+            string first = "alma";
+            string second = "mala";
+
+            Assert.AreEqual(true, Anagram.AreAnagrams(first, second));
+        }
+
+        [Test()]
+        public void NotAnagramInputs_Test()
+        {
+            string first = "alm";
+            string second = "mala";
+
+            Assert.AreEqual(false, Anagram.AreAnagrams(first, second));
+        }
+
+        [Test()]
+        public void UppercaseAnagramInputs_Test()
+        {
+            string first = "AlMa";
+            string second = "maLa";
+
+            Assert.AreEqual(true, Anagram.AreAnagrams(first, second));
+        }
+
+        [Test()]
+        public void IndentatedAnagramInputs_Test()
+        {
+            string first = "al, ma";
+            string second = "ma. la?";
+
+            Assert.AreEqual(true, Anagram.AreAnagrams(first, second));
+        }
+
+        [Test()]
+        public void IndentatedWithUppercaseAnagramInputs_Test()
+        {
+            string first = "aL, Ma";
+            string second = "mA. lA?";
+
+            Assert.AreEqual(true, Anagram.AreAnagrams(first, second));
+        }
+
+        [Test()]
+        public void DifferentNumberOfSameCharacters_Test()
+        {
+            string first = "aaalma";
+            string second = "mmalla";
+
+            Assert.AreEqual(false, Anagram.AreAnagrams(first, second));
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullValues_Test()
+        {
+            string first = null;
+            string second = null;
+
+            Anagram.AreAnagrams(first, second);
+        }
+
+    }
+}
