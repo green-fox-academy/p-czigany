e8bf7e70d4d6b57c7ae644094ae30039c42b6710
Peter Czigany <p.czigany@gmail.com>
2017-07-12 17:07:07 +0200

    Add Fibonacci term counter w/ test cases

---
+++ algorithms/Fibonacci.Test/FibonacciTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Fibonacci.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void FirstTerm_Test()
+        {
+            Assert.AreEqual(1, Fibonacci.FibonacciTerm(1));
+        }
+
+        [Test()]
+        public void SecondTerm_Test()
+        {
+            Assert.AreEqual(2, Fibonacci.FibonacciTerm(2));
+        }
+
+        [Test()]
+        public void ThirdTerm_Test()
+        {
+            Assert.AreEqual(3, Fibonacci.FibonacciTerm(3));
+        }
+
+        [Test()]
+        public void FourthTerm_Test()
+        {
+            Assert.AreEqual(5, Fibonacci.FibonacciTerm(4));
+        }
+
+    }
+}
+
---
+++ algorithms/Fibonacci/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        public static int FibonacciTerm(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else if (number == 2)
+            {
+                return 2;
+            }
+            else
+            {
+                return FibonacciTerm(number - 1) + FibonacciTerm(number - 2);
+            }
+        }
+    }
+}
+
