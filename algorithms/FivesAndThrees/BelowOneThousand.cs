aad3f0e7df404de4a60af0a2f9a07a0f6cb56153
Peter Czigany <p.czigany@gmail.com>
2017-07-12 15:34:00 +0200

    Add first solution
    - Add multiplies of 3 and 5 under 1000

---
+++ algorithms/FivesAndThrees/BelowOneThousand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FivesAndThrees
+{
+    public class BelowOneThousand
+    {
+        public int addAll()
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ algorithms/FivesAndThrees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FivesAndThrees
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
