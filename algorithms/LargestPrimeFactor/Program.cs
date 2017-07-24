90d34a08837519b692c4976beb65e1c61da32de9
Peter Czigany <p.czigany@gmail.com>
2017-07-24 10:56:38 +0200

    Add Euler Project task 3 initial commit

---
+++ algorithms/LargestPrimeFactor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LargestPrimeFactor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
