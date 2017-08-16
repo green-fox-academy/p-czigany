be399bf253bf5eaf294a45569c222ce804824b26
Peter Czigany <p.czigany@gmail.com>
2017-08-16 20:26:29 +0200

    Add file printing exercise

---
+++ week-03/day-1/src/ExPrintEachLine.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExPrintEachLine {
+
+  // Write a program that opens a file called "my-file.txt", then prints
+  // each of lines form the file.
+  // If the program is unable to read the file (for example it does not exists),
+  // then it should print an error message like: "Unable to read file: my-file.txt"
+
+  public static void main(String[] args) {
+    printFile("assets/my-file.txt");
+  }
+
+  private static void printFile(String stringPath) {
+    try {
+      Path filePath = Paths.get(stringPath);
+      List<String> lines = Files.readAllLines(filePath);
+      for (String line : lines) {
+        System.out.println(line);
+      }
+    } catch (IOException ex) {
+      System.out.println("Uh-oh, could not read the file! " + Arrays.toString(ex.getStackTrace()));
+    }
+  }
+}
