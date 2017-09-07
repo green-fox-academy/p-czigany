c504b0853475c6fcb57bcab67595e9a32126e250
Peter Czigany <p.czigany@gmail.com>
2017-09-07 15:15:57 +0200

    Move test class to test folder

--- week-04/day-1/test/CounterTest.java
+++ week-04/day-1/test/CounterTest.java
@@ -1,43 +1,41 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
  * Created by peter on 2017.04.03..
  */
-public class CounterTest {
+class CounterTest {
 
-  Counter c = new Counter();
-
+  private Counter c = new Counter();
 
   @Test
   void addMore() {
     c.add(5);
-    assertEquals(5, c.get());
+    Assertions.assertEquals(5, c.get());
   }
 
   @Test
   void addOne() {
     c.add();
-    assertEquals(1, c.get());
+    Assertions.assertEquals(1, c.get());
   }
 
   @Test
   void getZero() {
-    assertEquals(0, c.get());
+    Assertions.assertEquals(0, c.get());
   }
 
   @Test
   void getInit() {
     Counter c = new Counter(7);
-    assertEquals(7, c.get());
+    Assertions.assertEquals(7, c.get());
   }
 
   @Test
   void resetToZero() {
     c.add();
     c.reset();
-    assertEquals(0, c.get());
+    Assertions.assertEquals(0, c.get());
   }
 
   @Test
@@ -45,6 +43,6 @@ public class CounterTest {
     Counter c = new Counter(7);
     c.add(5);
     c.reset();
-    assertEquals(7, c.get());
+    Assertions.assertEquals(7, c.get());
   }
 }
