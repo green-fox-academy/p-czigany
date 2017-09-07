d41bb45b906e5355cc0149358a9fe46862367460
Peter Czigany <p.czigany@gmail.com>
2017-09-07 16:41:13 +0200

    Add ship fields and fillShip method

--- week-04/day-1/src/Ship.java
+++ week-04/day-1/src/Ship.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Ship {
 
+  private List<Pirate> crew;
+  private Pirate captain;
+
+  Ship() {
+    this.crew = new ArrayList<>();
+  }
+
+  void fillShip(){
+    captain = new Pirate();
+    int numberOfPirates = new Random().nextInt(100) + 1;
+    for (int i = 0; i < numberOfPirates; i++) {
+      crew.add(new Pirate());
+    }
+  }
 }
