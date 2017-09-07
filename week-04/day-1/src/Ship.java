ac3a37f7c7bc3bbaf911e8316eedbb4ce6c83d61
Peter Czigany <p.czigany@gmail.com>
2017-09-07 17:44:03 +0200

    Add battle method

--- week-04/day-1/src/Ship.java
+++ week-04/day-1/src/Ship.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Ship {
+class Ship {
 
   private List<Pirate> crew;
   private Pirate captain;
@@ -43,5 +43,55 @@ public class Ship {
       }
     }
     return lives;
+  }
+  
+  boolean battle(Ship enemyShip) {
+    int aliveAttackerPirates = countLivingCrew();
+    int aliveEnemyPirates = enemyShip.countLivingCrew();
+    Pirate enemyCaptain = enemyShip.captain;
+    int attackerScore = aliveAttackerPirates - captain.getIntoxication();
+    int enemyScore = aliveEnemyPirates - enemyCaptain.getIntoxication();
+    Ship winner = (attackerScore > enemyScore) ? this : enemyShip;
+    Ship loser = (winner == this) ? enemyShip : this;
+    loser.casualties();
+    winner.party();
+    return (winner == this);
+  }
+
+  private void casualties() {
+    int foughtInBattle = countLivingCrew();
+    int deaths = new Random().nextInt(foughtInBattle) + 1;
+    int i = 0;
+    while (i < deaths) {
+      if (crew.get(i).isAlive()) {
+        crew.get(i).die();
+        i++;
+      }
+    }
+  }
+
+  private void party() {
+    int rums = new Random().nextInt(countLivingCrew() * 2) + 1;
+    for (int i = 0; i < rums; i++) {
+      giveRumRandomly();
+    }
+  }
+
+  private void giveRumRandomly() {
+    boolean givenOut = false;
+    Random rnd = new Random();
+    while (!givenOut) {
+      int randomPirateIndex = rnd.nextInt(countLivingCrew() + 1) + 1;
+      if (randomPirateIndex == 0 && captain.isAlive() && captain.isConscious()) {
+        captain.drinkSomeRum();
+        givenOut = true;
+      } else {
+        Pirate rumTarget = crew.get(randomPirateIndex - 1);
+        if (rumTarget.isAlive() && rumTarget.isConscious()) {
+          rumTarget.drinkSomeRum();
+          givenOut = true;
+        }
+      }
+    }
   }
 }
