82c561f3c4904a2326ad74fa97035b798beebc06
Peter Czigany <p.czigany@gmail.com>
2017-09-07 16:02:30 +0200

    Teach Pirate to report, die and brawl others

--- week-04/day-1/src/Pirate.java
+++ week-04/day-1/src/Pirate.java
@@ -1,16 +1,71 @@
+import java.util.Random;
+
 public class Pirate {
 
   private int intoxication;
   private boolean conscious;
   private boolean alive;
+  private static final String DEAD = "They are dead.";
 
-  public Pirate() {
+  Pirate() {
     intoxication = 0;
     conscious = true;
     alive = true;
   }
 
   public void drinkSomeRum() {
+    if (!alive) {
+      System.out.println(DEAD);
+      return;
+    }
     intoxication++;
+  }
+
+  void howsItGoingMate() {
+    if (!alive) {
+      System.out.println(DEAD);
+      return;
+    }
+    if (intoxication < 5) {
+      System.out.println("\"Pour me anudder!\"");
+      return;
+    }
+    System.out.println("\"Arghh, I'ma Pirate. How d'ya d'ink its goin?\"");
+    conscious = false;
+  }
+
+  private void die() {
+    if (!alive) {
+      System.out.println(DEAD);
+      return;
+    }
+    alive = false;
+  }
+
+  void brawl(Pirate target) {
+    if (!target.alive) {
+      System.out.println("They are already dead.");
+      return;
+    }
+    if (!target.conscious) {
+      target.die();
+      return;
+    }
+    Random r = new Random();
+    int d3roll = r.nextInt(3) + 1;
+    switch (d3roll) {
+      case 1:
+        alive = false;
+        break;
+      case 2:
+        target.alive = false;
+        break;
+      case 3:
+        conscious = false;
+        target.conscious = false;
+        break;
+      default:
+        break;
+    }
   }
 }
