6f2cd35fbfbb895df26e0bd3b23fc7c1f5c16e33
Peter Czigany <p.czigany@gmail.com>
2017-09-07 16:19:37 +0200

    Add Pirate Brawl app for testing

---
+++ week-04/day-1/src/PirateApp.java
@@ -0,0 +1,10 @@
+public class PirateApp {
+
+  public static void main(String[] args) {
+    Pirate feri = new Pirate();
+    Pirate dini = new Pirate();
+    feri.brawl(dini);
+    feri.howsItGoingMate();
+    dini.howsItGoingMate();
+  }
+}
