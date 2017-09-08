e38b2514f50504f0173f8cbd0becd7987f5bab85
Peter Czigany <p.czigany@gmail.com>
2017-09-08 20:55:27 +0200

    Add basic version of the game

--- week-04/day-3/src/CowsAndBulls.java
+++ week-04/day-3/src/CowsAndBulls.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class CowsAndBulls {
 
@@ -43,5 +44,40 @@ class CowsAndBulls {
       random4Digits.append(rnd.nextInt(10));
     }
     return random4Digits.toString();
+  }
+
+  String guess() {
+    StringBuilder answer = new StringBuilder();
+    int cows = 0;
+    int bulls = 0;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Guess! (Type 4 digits.)");
+    String guess = scan.nextLine();
+    for (int i = 0; i < 4; i++) {
+      char examinedLetter = guess.charAt(i);
+      if (examinedLetter == number.charAt(i)) {
+        cows++;
+      } else if (number.contains(String.valueOf(examinedLetter))) {
+        bulls++;
+      }
+    }
+    answer
+        .append("Cows: ")
+        .append(cows)
+        .append("\n")
+        .append("Bulls: ")
+        .append(bulls)
+        .append("\n");
+    if (cows == 4) {
+      gameState = GameState.FINISHED;
+    }
+    return answer.toString();
+  }
+
+  public static void main(String[] args) {
+    CowsAndBulls myGame = new CowsAndBulls();
+    while (!myGame.gameState.equals(GameState.FINISHED)) {
+      System.out.println(myGame.guess());
+    }
   }
 }
