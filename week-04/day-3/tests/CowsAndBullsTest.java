bd4434ec5ddde7e892a4b4160ab3d25205c74631
Peter Czigany <p.czigany@gmail.com>
2017-09-09 13:18:59 +0200

    Add test for the input validator function

--- week-04/day-3/tests/CowsAndBullsTest.java
+++ week-04/day-3/tests/CowsAndBullsTest.java
@@ -29,4 +29,18 @@ class CowsAndBullsTest {
     assertEquals(GameState.PLAYING, myGame.getGameState());
     assertEquals(0, myGame.getGuesses());
   }
+
+  @Test
+  void guessValidatorTest() {
+    assertTrue(myGame.validateGuess("0000"));
+    assertTrue(myGame.validateGuess("1000"));
+    assertTrue(myGame.validateGuess("0001"));
+    assertTrue(myGame.validateGuess("9356"));
+    assertFalse(myGame.validateGuess("000"));
+    assertFalse(myGame.validateGuess("100"));
+    assertFalse(myGame.validateGuess("001"));
+    assertFalse(myGame.validateGuess("999"));
+    assertFalse(myGame.validateGuess(""));
+    assertFalse(myGame.validateGuess(null));
+  }
 }
