c9b417c34906b3ab438a2f38befb7a254657091f
Peter Czigany <p.czigany@gmail.com>
2017-09-09 17:34:11 +0200

    Add CVV checksum number generator testcase

---
+++ week-06/day-1/test/creditcard/CreditCardTest.java
@@ -0,0 +1,17 @@
+package creditcard;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class CreditCardTest {
+
+  private CreditCard myCreditCard = new CreditCard();
+
+  @Test
+  void countCVV(){
+    assertEquals(69, myCreditCard.cumeSumCVV("6765350286622461"));
+  }
+
+
+}
