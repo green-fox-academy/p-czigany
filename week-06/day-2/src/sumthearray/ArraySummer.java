ca5be6b1261ce37d257d4c09c5b71f6385ced18c
Peter Czigany <p.czigany@gmail.com>
2017-09-13 21:46:06 +0200

    Polish generic array summation exercise

--- week-06/day-2/src/sumthearray/ArraySummer.java
+++ week-06/day-2/src/sumthearray/ArraySummer.java
@@ -2,9 +2,6 @@ package sumthearray;
 
 import java.util.Arrays;
 
-/**
- * Created by peter on 2017.04.19..
- */
 public class ArraySummer {
 
   public <T extends Number> double sumArray(T[] inputArray, int l) {
